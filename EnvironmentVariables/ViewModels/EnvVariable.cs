b7d65f59d6a6a50d8a24859719593eeb70a741b1
Stefan Markovic <stefan@janeasystems.com>
2022-04-01 14:48:50 +0200

    [WIP] init

---
+++ EnvironmentVariables/ViewModels/EnvVariable.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace EnvironmentVariables.ViewModels
+{
+    internal class EnvVariable
+    {
+        public EnvVariable(string Name, ObservableCollection<string> Values)
+        {
+            this.Name = Name;
+            this.Values = Values;
+        }
+
+        public string Name { get; }
+        public ObservableCollection<string> Values{ get; } = new ObservableCollection<string>();
+    }
+}
---
+++ EnvironmentVariables/ViewModels/EnvVariables.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnvironmentVariables.ViewModels
+{
+    internal class EnvVariables : INotifyPropertyChanged
+    {
+        public EnvVariables()
+        {
+            LoadSystemEnvVariables();
+        }
+
+        private void LoadSystemEnvVariables()
+        {
+            var machineEnvVariables = Environment.GetEnvironmentVariables(EnvironmentVariableTarget.Machine);
+            foreach (DictionaryEntry envVariable in machineEnvVariables)
+            {
+                string key = envVariable.Key as string;
+                string value = envVariable.Value as string;
+                if (string.IsNullOrEmpty(key) || string.IsNullOrEmpty(value))
+                {
+                    continue;
+                }
+
+                ObservableCollection<string> values = new ObservableCollection<string>();
+                var splitValues = value.Split(';');
+                foreach(string valueSplit in splitValues)
+                {
+                    values.Add(valueSplit);
+                }
+                SystemEnvVariables.Add(new EnvVariable(key, values));
+            }
+        }
+
+        public ObservableCollection<EnvVariable> SystemEnvVariables { get; } = new ObservableCollection<EnvVariable>();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
